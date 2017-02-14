6c77fbac7b8d7809bb1a654e6d01ad0f59c18a91
ocunyara <smolin_a@mail.ua>
2017-02-14 17:23:13 +0300

    new2

--- new/script/main.js
+++ new/script/main.js
@@ -56,6 +56,19 @@ $(document).ready(function() {
     }) 
 
 
+    $("nav").on("click", "a", function(event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1000);
+    });
+
+    $('nav:after').toggle( function() {
+        $( "nav ul"  ).show( );
+      }, function() {
+        $( "nav ul"  ).hide(  );
+  });
+
 
 
       function initialize() {
