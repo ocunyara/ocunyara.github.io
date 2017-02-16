791dc00be8578918d7523d6dcf911740f8187e54
ocunyara <smolin_a@mail.ua>
2017-02-16 15:43:33 +0300

    fix

--- new/script/main.js
+++ new/script/main.js
@@ -64,11 +64,29 @@ $(document).ready(function() {
         $('body,html').animate({ scrollTop: top }, 1000);
     });
 
+    var ravenous = function() {
+        if (window.matchMedia('(max-width: 992px)').matches) {
+            $('nav').click(function(){
+                $('nav ul').toggle();
+            });
+        }
+    };
 
-    $('nav').click(function(){
-        $('nav ul').toggle();
+ 
+
+
+    var nav = $('header');
+    var height = $('header').height();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            nav.addClass("f-nav");
+        } else {
+            nav.removeClass("f-nav");
+        }
     });
-
+      $(window).resize(ravenous);
+      // Call the function
+      ravenous(); 
 
       function initialize() {
         var styles = [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}]
